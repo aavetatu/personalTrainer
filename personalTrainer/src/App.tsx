e81ef5f32cf7e5acf495c3b9a66795c84bc3d186
Tatu Heikkinen <52470440+aavetatu@users.noreply.github.com>
2025-04-29 20:21:18 +0300

    Show customer info in AgGrid

--- personalTrainer/src/App.tsx
+++ personalTrainer/src/App.tsx
@@ -1,9 +1,30 @@
 import Customerlist from './components/Customerlist.tsx';
+import Container from '@mui/material/Container';
+
+
+
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+import AppBar from '@mui/material/AppBar';
+
+
+import Toolbar from '@mui/material/Toolbar';
+
+
+import Typograpy from '@mui/material/Typography';
 
 function App() {
-
   return (
-    <Customerlist />
+    <Container maxWidth="lg">
+      <AppBar position="static" >
+        <Toolbar>
+          <Typograpy variant="h6">Customers</Typograpy>
+        </Toolbar>
+      </AppBar>
+      <Customerlist />
+      <CssBaseline />
+    </Container>
   )
 }
 
--- personalTrainer/src/components/Customerlist.tsx
+++ personalTrainer/src/components/Customerlist.tsx
@@ -1,9 +1,29 @@
 import { AgGridReact } from "ag-grid-react";
-import { AllCommunityModule, ModuleRegistry, ColDef, ICellRendererParams, themeMaterial } from 'ag-grid-community';
-import { useState } from "react";
+import { AllCommunityModule, ModuleRegistry, ColDef } from 'ag-grid-community';
+import { useState, useEffect } from "react";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
 
 type Customer = {
-	name: string;
+	firstname: string;
+	lastname: string;
+	streetaddress: string;
+	postcode: string;
+	city: string;
+	email: string;
+	phone: string;
+	//	_links: {
+	//		self: {
+	//			href: string;
+	//		},
+	//		customer: {
+	//			href: string;
+	//		},
+	//		trainings: {
+	//			href: string;
+	//		},
+	//	}
 }
 
 export default function Customerlist() {
@@ -11,31 +31,40 @@ export default function Customerlist() {
 	const [customers, setCustomers] = useState<Customer[]>([]);
 
 	const [columnDefs] = useState<ColDef<Customer>[]>([
-		{
-			field: 'name',
-			headerName: 'Name',
-			cellRenderer: (params: ICellRendererParams) => {
-				return <span>{params.value}</span>;
-			},
-		},
-	]);
+		{ field: 'firstname' },
+		{ field: 'lastname' },
+		{ field: 'streetaddress' },
+		{ field: 'postcode' },
+		{ field: 'city' },
+		{ field: 'email' },
+		{ field: 'phone' },
+	])
 
-	const addCustomer = () => {
-		console.log("test")
-	};
+	useEffect(() => {
+		fetchCustomers();
+	}, []);
+
+	const fetchCustomers = () => {
+		console.log("fetching from: ", import.meta.env.VITE_API_URL + "customers")
+		fetch(import.meta.env.VITE_API_URL + "customers")
+			.then(response => {
+				if (!response.ok)
+					throw new Error('Error in fetch');
+				return response.json();
+			})
+			.then(data => {
+				console.log(data);
+				setCustomers(data._embedded.customers);
+			})
+			.catch(err => console.error(err));
+	}
 
 	return (
 		<>
-			<input
-				placeholder="Name"
-
-			/>
-			<button onClick={() => addCustomer}>Add Customer</button>
 			<div style={{ width: '90%', height: 500 }}>
 				<AgGridReact
 					rowData={customers}
 					columnDefs={columnDefs}
-					modules={[AllCommunityModule]}
 				/>
 			</div>
 		</>
